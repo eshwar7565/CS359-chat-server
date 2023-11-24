84b89c82cdcde56c795e9acc404afa9a6b5f1a43
eshwar <eshwarrachakonda02@gmail.com>
2023-11-24 16:06:22 +0530

    Please update zegocloud config details

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,22 @@
+const AudioCall = require("../models/audioCall");
 const friendRequest = require("../models/friendRequest");
 const User = require("../models/user");
 
 const filterObj = require("../utils/filterObj");
 const catchAsync = require("../utils/catchAsync.js");
+
+const { generateToken04 } = require("./zegoServerAssistant");
+
+
+// Please change appID to your appId, appid is a number
+// Example: 1234567890
+const appID = process.env.ZEGO_APP_ID; // type: number
+
+
+// Please change serverSecret to your serverSecret, serverSecret is string
+// Example：'sdfsdfsd323sdfsdf'
+const serverSecret = process.env.ZEGO_SERVER_SECRET; // type: 32 byte length string
+
 
 exports.getMe = catchAsync(async (req, res, next) => {
   res.status(200).json({
@@ -92,3 +106,151 @@ exports.getFriends = catchAsync(async (req, res, next) => {
 
 
 /// Call logs and call actions
+
+
+exports.generateZegoToken = catchAsync(async (req, res, next) => {
+  try {
+    const { userId, room_id } = req.body;
+
+    console.log(userId, room_id, "from generate zego token");
+
+    const effectiveTimeInSeconds = 3600; //type: number; unit: s; token expiration time, unit: second
+    const payloadObject = {
+      room_id, // Please modify to the user's roomID
+      // The token generated allows loginRoom (login room) action
+      // The token generated in this example allows publishStream (push stream) action
+      privilege: {
+        1: 1, // loginRoom: 1 pass , 0 not pass
+        2: 1, // publishStream: 1 pass , 0 not pass
+      },
+      stream_id_list: null,
+    }; //
+    const payload = JSON.stringify(payloadObject);
+    // Build token
+    const token = generateToken04(
+      appID * 1, // APP ID NEEDS TO BE A NUMBER
+      userId,
+      serverSecret,
+      effectiveTimeInSeconds,
+      payload
+    );
+    res.status(200).json({
+      status: "success",
+      message: "Token generated successfully",
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+exports.startAudioCall = catchAsync(async (req, res, next) => {
+  const from = req.user._id;
+  const to = req.body.id;
+
+  const from_user = await User.findById(from);
+  const to_user = await User.findById(to);
+
+  // create a new call audioCall Doc and send required data to client
+  const new_audio_call = await AudioCall.create({
+    participants: [from, to],
+    from,
+    to,
+    status: "Ongoing",
+  });
+
+  res.status(200).json({
+    data: {
+      from: to_user,
+      roomID: new_audio_call._id,
+      streamID: to,
+      userID: from,
+      userName: from,
+    },
+  });
+});
+
+
+exports.getCallLogs = catchAsync(async (req, res, next) => {
+  const user_id = req.user._id;
+
+  const call_logs = [];
+
+  const audio_calls = await AudioCall.find({
+    participants: { $all: [user_id] },
+  }).populate("from to");
+
+  // const video_calls = await VideoCall.find({
+  //   participants: { $all: [user_id] },
+  // }).populate("from to");
+
+  console.log(audio_calls, 
+    // video_calls
+    );
+
+  for (let elm of audio_calls) {
+    const missed = elm.verdict !== "Accepted";
+    if (elm.from._id.toString() === user_id.toString()) {
+      const other_user = elm.to;
+
+      // outgoing
+      call_logs.push({
+        id: elm._id,
+        img: other_user.avatar,
+        name: other_user.firstName,
+        online: true,
+        incoming: false,
+        missed,
+      });
+    } else {
+      // incoming
+      const other_user = elm.from;
+
+      // outgoing
+      call_logs.push({
+        id: elm._id,
+        img: other_user.avatar,
+        name: other_user.firstName,
+        online: true,
+        incoming: false,
+        missed,
+      });
+    }
+  }
+
+  // for (let element of video_calls) {
+  //   const missed = element.verdict !== "Accepted";
+  //   if (element.from._id.toString() === user_id.toString()) {
+  //     const other_user = element.to;
+
+  //     // outgoing
+  //     call_logs.push({
+  //       id: element._id,
+  //       img: other_user.avatar,
+  //       name: other_user.firstName,
+  //       online: true,
+  //       incoming: false,
+  //       missed,
+  //     });
+  //   } else {
+  //     // incoming
+  //     const other_user = element.from;
+
+  //     // outgoing
+  //     call_logs.push({
+  //       id: element._id,
+  //       img: other_user.avatar,
+  //       name: other_user.firstName,
+  //       online: true,
+  //       incoming: false,
+  //       missed,
+  //     });
+  //   }
+  // }
+
+  res.status(200).json({
+    status: "success",
+    message: "Call Logs Found successfully!",
+    data: call_logs,
+  });
+});
